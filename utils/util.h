39e3136d41477cf35eb8ff1dcfa46f090a190209
development-with-benjamin <bhalilovic.dev@gmail.com>
2023-01-29 16:23:19 +0100

    Update docstring

--- utils/util.h
+++ utils/util.h
@@ -7,9 +7,9 @@ namespace util
 {
     void tokenize(std::string const &str, const char delim, std::vector<std::string> &out);
 
-    /* This function checks if all values within this string are occuring only once.
-    * const std::string& string: string to be checked on uniqueness.
-    * return: True if all characters in string occur only once.
+    /* 
+    * This function checks if all values within this string are occuring only once.
+    * Returns true if all characters in string occur only once.
     */
     bool is_unique(const std::string& string);
     bool is_unique_ascii(const char* string, const size_t n);
