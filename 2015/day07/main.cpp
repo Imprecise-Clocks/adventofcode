631a292e64a586d81092afc8e5ae42519ea9e1e5
Benjamin Halilovic <bhalilovic.dev@gmail.com>
2023-04-27 15:13:53 +0200

    Create register class for solution

--- 2015/day07/main.cpp
+++ 2015/day07/main.cpp
@@ -5,29 +5,65 @@
 
 class Register {
 public:
-    Register(const std::string& name, uint16_t value) : name(name), value(value) {}
+    Register(const std::string& name, uint16_t value) : name(name), value(value), is_initialized(true) {}
+    Register(const std::string& name) : name(name) {}
 
     Register& operator =(const Register& other)
     {
         this->value = other.value;
-        return this;
+        this->is_initialized = true;
+        return *this;
     }
 
     Register& operator =(const uint16_t value)
     {
         this->value = value;
-        return this;
+        this->is_initialized = true;
+        return *this;
     }
 
-    Register operator &(const Register& other) const
+    uint16_t operator &(const Register& other) const  
     {
-
+        return this->value & other.value;
     }
+
+    uint16_t operator |(const Register& other) const
+    {
+        return this->value | other.value;
+    }
+
+    uint16_t operator &(const uint16_t value) const
+    {
+        return this->value & value;
+    }
+
+    uint16_t operator |(const uint16_t value) const
+    {
+        return this->value | value;
+    }
+
+    uint16_t operator !() const
+    {
+        return !this->value;
+    }
+
+    bool is_initialized = false;
+    std::string name;
 
 private:
-    std::string name;
     uint16_t value;
 };
+
+template <typename T>
+T parse(std::string& text)
+{
+    // [0-9]+ -> [a-z]+
+    // ([a-z]+|[0-9]+) AND ([a-z]+|[0-9]+) -> [a-z]+
+    // ([a-z]+|[0-9]+) OR ([a-z]+|[0-9]+) -> [a-z]+
+    // ([a-z]+|[0-9]+) LSHIFT [0-9]+ -> [a-z]+
+    // ([a-z]+|[0-9]+) RSHIFT [0-9]+ -> [a-z]+
+    // NOT [a-z]+ -> [a-z]+
+}
 
 size_t part1()
 {  
