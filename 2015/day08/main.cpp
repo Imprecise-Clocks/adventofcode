84fc126d560679f0c9b9a5cd2fb14f1952a8192e
development-with-benjamin <bhalilovic.dev@gmail.com>
2024-12-04 16:09:25 +0100

    Add changes to day 8 2015

--- 2015/day08/main.cpp
+++ 2015/day08/main.cpp
@@ -4,24 +4,31 @@
 #include <regex>
 #include "../../utils/util.h"
 
+int32_t total_single_quote_count = 0;
+int32_t total_double_back_count = 0;
+int32_t total_hex_number_count = 0;
 
 size_t string_size_regex(const std::string& str) {
-    // std::string temp = str.substr(1, str.length() - 2);
+    std::string temp = str.substr(1, str.length() - 2);
     std::regex double_back("\\\\\\\\"); // (?!(x[0-9a-f]{2}))
     std::regex single_quote("\\\"");
     std::regex hex_special("\\\\x[0-9a-f]{2}");
 
     auto single_quote_count(std::distance(
-        std::sregex_iterator(str.begin(), str.end(), single_quote),
+        std::sregex_iterator(temp.begin(), temp.end(), single_quote),
         std::sregex_iterator()));
     auto double_back_count(std::distance(
-        std::sregex_iterator(str.begin(), str.end(), double_back),
+        std::sregex_iterator(temp.begin(), temp.end(), double_back),
         std::sregex_iterator()));
     auto hex_count(std::distance(
-        std::sregex_iterator(str.begin(), str.end(), hex_special),
+        std::sregex_iterator(temp.begin(), temp.end(), hex_special),
         std::sregex_iterator()));
 
-    return str.size() - (single_quote_count + double_back_count + hex_count * 3 + 2);
+    total_double_back_count += double_back_count;
+    total_single_quote_count += single_quote_count;
+    total_hex_number_count += hex_count;
+
+    return temp.size() - (single_quote_count + double_back_count + hex_count * 3);
 }
 
 size_t part1()
@@ -29,18 +36,18 @@ size_t part1()
     std::ifstream file;
     file.open("input.txt");
     std::string buffer;
-    size_t sum = 0;
-    size_t memory_size;
-    size_t string_size;
+    size_t memory_size = 0;
+    size_t string_size = 0;
+    size_t difference = 0;
 
     while(std::getline(file, buffer)) {
         memory_size = buffer.size();
         string_size = string_size_regex(buffer);
-        sum += memory_size - string_size;
+        difference += memory_size - string_size;
     }
     file.close();
 
-    return sum;
+    return difference;
 }
 
 size_t part2()
--- 2016/day01/main.cpp
+++ 2016/day01/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <array>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -9,14 +10,14 @@
 
 int32_t part1()
 {
-    std::ifstream file;
-    file.open("input.txt");
-    std::string buffer;
+	std::ifstream file;
+	file.open("input.txt");
+	std::string buffer;
 	std::getline(file, buffer);
-    file.close();
+	file.close();
 
 	std::vector<std::string> instructions;
-	std::vector<int32_t> directions = {0, 0, 0, 0};
+	std::array<int32_t, 4> position = {0, 0, 0, 0};
 	util::tokenize(buffer, ',', instructions);
 	int32_t current_direction = 0;
 	for(std::string& element: instructions) {
@@ -24,37 +25,83 @@ int32_t part1()
 			element = element.substr(1, element.size() -1);
 	}
 
-    for(std::string& direction : instructions) {
-   		if(direction[0] == 'R') {
+	for(std::string& direction : instructions) {
+		if(direction[0] == 'R') {
 			current_direction = (current_direction + 1) % 4;
 			int32_t walk = std::stoi(direction.substr(1, direction.size() - 1));
-			directions[current_direction] += walk;
+			position[current_direction] += walk;
 		}
-   		if(direction[0] == 'L') {
-			current_direction = (current_direction - 1) % 4;
+		if(direction[0] == 'L') {
+			current_direction = (current_direction - 1 == -1) ? 3 : current_direction - 1;
 			int32_t walk = std::stoi(direction.substr(1, direction.size() - 1));
-			directions[current_direction] += walk;
+			position[current_direction] += walk;
 		}
-    }
-	int32_t distance = std::max(directions.at(0), directions.at(2)) - std::min(directions.at(0), directions.at(2));
-	distance += std::max(directions.at(1), directions.at(3)) - std::min(directions.at(1), directions.at(3));
-    return distance;
+	}
+	int32_t distance = (int32_t) std::max(position.at(0), position.at(2)) - (int32_t) std::min(position.at(0), position.at(2));
+	distance += (int32_t) std::max(position.at(1), position.at(3)) - (int32_t) std::min(position.at(1), position.at(3));
+	return distance;
 }
 
 int32_t part2()
 {    
-    std::ifstream file;
-    file.open("input.txt");
-    std::string buffer;
-    std::getline(file, buffer);
-    file.close();
-	
+	std::ifstream file;
+	file.open("input.txt");
+	std::string buffer;
+	std::getline(file, buffer);
+	file.close();
+
+	std::vector<std::string> instructions;
+	std::array<int32_t, 2> position = {0, 0};
+	util::tokenize(buffer, ',', instructions);
+	int32_t current_direction = 0;
+	for(std::string& element: instructions) {
+		// remove leading white spaces
+		if (element[0] == ' ')
+			element = element.substr(1, element.size() -1);
+	}
+
+	int32_t gradient;
+	std::vector<std::array<int32_t, 2>> positions = {};
+	for(std::string& direction: instructions) {
+		positions.push_back(position);
+		if(direction[0] == 'R') {
+			current_direction = (current_direction + 1) % 4;
+			if(current_direction == 0 || current_direction == 2) {
+				gradient = (current_direction == 0) ? 1 : -1; 
+				current_direction = 0;
+			}
+			else {
+				gradient = (current_direction == 1) ? 1 : -1; 
+				current_direction = 1;
+			}
+			int32_t walk = std::stoi(direction.substr(1, direction.size() - 1));
+			position[current_direction] += walk * gradient;
+		}
+		if(direction[0] == 'L') {
+			current_direction = (current_direction - 1 == -1) ? 3 : current_direction - 1;
+			if(current_direction == 0 || current_direction == 2) {
+				gradient = (current_direction == 0) ? 1 : -1; 
+				current_direction = 0;
+			}
+			else {
+				gradient = (current_direction == 1) ? 1 : -1; 
+				current_direction = 1;
+			}
+			int32_t walk = std::stoi(direction.substr(1, direction.size() - 1));
+			position[current_direction] += walk * gradient;
+		}
+		for(auto& p : positions) {
+			if(position[0] == p[0] && position[1] == p[1]) {
+				return position[0] + position[1];
+			}
+		}
+	}
 	return -1;
 }
 
-int main() 
+int main(int argc, char** argv) 
 {
-    std::cout << "The answer to part 1: " << part1() << std::endl;
-    std::cout << "The answer to part 2: " << part2() << std::endl;
-    return 0;
+	std::cout << "The answer to part 1: " << part1() << std::endl;
+	std::cout << "The answer to part 2: " << part2() << std::endl;
+	return 0;
 }
