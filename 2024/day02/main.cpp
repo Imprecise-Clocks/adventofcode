f4fa7860fd3751810bdfc472ba512163a727a831
development-with-benjamin <bhalilovic.dev@gmail.com>
2024-12-04 16:17:59 +0100

    Update gitignore

--- 2024/day02/main.cpp
+++ 2024/day02/main.cpp
@@ -2,10 +2,8 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
-#include <array>
 #include <stdint.h>
 #include <vector>
-#include <map>
 
 #include "../../utils/util.h"
 
@@ -24,20 +22,20 @@ int part1()
     file.open("input.txt");
     std::string buffer;
     std::vector<int> tokens;
+    std::vector<int> reverse;
     int safe_count = 0;
 
     while(std::getline(file, buffer)) {
         util::tokenize(buffer, ' ', tokens);
-        std::vector<int> reverse = tokens;
-        std::reverse(reverse.begin(), reverse.end());
+        std::reverse_copy(std::begin(tokens), std::end(tokens), std::begin(reverse));
         if(is_increasing(tokens, 3, 1) || is_increasing(reverse, 3, 1)) {
             safe_count++;
         }
-        
         tokens.clear();
+        reverse.clear();
     }
-
     file.close();
+
     return safe_count;
 }
 
@@ -47,32 +45,30 @@ int part2()
     file.open("input.txt");
     std::string buffer;
     std::vector<int> tokens;
+    std::vector<int> reverse;
     int safe_count = 0;
 
     while(std::getline(file, buffer)) {
         util::tokenize(buffer, ' ', tokens);
-        std::vector<int> reverse = tokens;
-        std::reverse(reverse.begin(), reverse.end());
-        
+        std::reverse_copy(std::begin(tokens), std::end(tokens), std::end(reverse));
         if(is_increasing(tokens, 3, 1) || is_increasing(reverse, 3, 1)) {
             safe_count++;
         }
         else {
             for (size_t i = 0; i < tokens.size(); ++i) {
+                reverse.clear();
+
                 std::vector<int> reduced_tokens = tokens;
                 reduced_tokens.erase(reduced_tokens.begin() + i);
-                std::vector<int> reduced_reverse = reduced_tokens;
-                std::reverse(reduced_reverse.begin(), reduced_reverse.end());
-
-                
-                if(is_increasing(reduced_tokens, 3, 1) || is_increasing(reduced_reverse, 3, 1)) {
+                std::reverse_copy(std::begin(reduced_tokens), std::end(reduced_tokens), std::begin(reverse));
+                if(is_increasing(reduced_tokens, 3, 1) || is_increasing(reverse, 3, 1)) {
                     safe_count++;
                     break;
                 }
             }
         }
-        
         tokens.clear();
+        reverse.clear();
     }
 
     file.close();
