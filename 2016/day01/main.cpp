1051a3bd4a73a0196e7d356fc138286331480d49
development-with-benjamin <bhalilovic.dev@gmail.com>
2024-12-09 19:42:16 +0100

    Update gitignore

--- 2016/day01/main.cpp
+++ 2016/day01/main.cpp
@@ -1,26 +1,23 @@
-#include <cstdint>
 #include <array>
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <stdint.h>
 #include <unordered_map>
 #include <vector>
-#include <algorithm>
 #include "../../utils/util.h"
 
-// Hash function for std::array<int32_t, 2>
+// Hash function for std::array<int, 2>
 struct ArrayHash {
-    std::size_t operator()(const std::array<int32_t, 2>& arr) const {
+    std::size_t operator()(const std::array<int, 2>& arr) const {
         std::size_t seed = 0;
         for (const auto& elem : arr) {
-            seed ^= std::hash<int32_t>()(elem) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+            seed ^= std::hash<int>()(elem) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
         }
         return seed;
     }
 };
 
-int32_t part1()
+int part1()
 {
 	std::ifstream file;
 	file.open("input.txt");
@@ -29,9 +26,9 @@ int32_t part1()
 	file.close();
 
 	std::vector<std::string> instructions;
-	std::array<int32_t, 4> position = {0, 0, 0, 0};
+	std::array<int, 4> position = {0, 0, 0, 0};
 	util::tokenize(buffer, ',', instructions);
-	int32_t current_direction = 0;
+	int current_direction = 0;
 	for(std::string& element: instructions) {
 		if (element[0] == ' ')
 			element = element.substr(1, element.size() -1);
@@ -44,15 +41,15 @@ int32_t part1()
 		if(direction[0] == 'L') {
 			current_direction = (current_direction + 3) % 4;
 		}
-		int32_t walk = std::stoi(direction.substr(1, direction.size() - 1));
+		int walk = std::stoi(direction.substr(1, direction.size() - 1));
 		position[current_direction] += walk;
 	}
-	int32_t distance = (int32_t) std::max(position.at(0), position.at(2)) - (int32_t) std::min(position.at(0), position.at(2));
-	distance += (int32_t) std::max(position.at(1), position.at(3)) - (int32_t) std::min(position.at(1), position.at(3));
+	int distance = (int) std::max(position.at(0), position.at(2)) - (int) std::min(position.at(0), position.at(2));
+	distance += (int) std::max(position.at(1), position.at(3)) - (int) std::min(position.at(1), position.at(3));
 	return distance;
 }
 
-int32_t part2()
+int part2()
 {    
 	std::ifstream file;
 	file.open("input.txt");
@@ -61,9 +58,9 @@ int32_t part2()
 	file.close();
 
 	std::vector<std::string> instructions;
-	std::array<int32_t, 2> position = {0, 0};
-	std::unordered_map<std::array<int32_t, 2>, int32_t, ArrayHash> locations;
-	int32_t current_direction = 0;
+	std::array<int, 2> position = {0, 0};
+	std::unordered_map<std::array<int, 2>, int, ArrayHash> locations;
+	int current_direction = 0;
 
 	util::tokenize(buffer, ',', instructions);
 	for(std::string& element: instructions) {
@@ -78,17 +75,17 @@ int32_t part2()
 		if(direction[0] == 'L') {
 			current_direction = (current_direction + 3) % 4;
 		}
-		int32_t gradient = 1;
+		int gradient = 1;
 		if(current_direction > 1) {
 			current_direction -= 2;
 			gradient = -1;
 		}
-		int32_t walk = std::stoi(direction.substr(1, direction.size() - 1));
+		int walk = std::stoi(direction.substr(1, direction.size() - 1));
 		while(walk--) {
 			position[current_direction] += gradient;
 			locations[position]++;
 			if(locations[position] == 2) {
-				int32_t distance = std::abs(position[0]) + std::abs(position[1]);
+				int distance = std::abs(position[0]) + std::abs(position[1]);
 				return distance;
 			}
 		}
