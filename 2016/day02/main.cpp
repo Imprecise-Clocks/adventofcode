48c94b3d9bacf4e860b72d4a7b336d679e62cb3e
Benjamin Halilovic <bhalilovic.dev@gmail.com>
2025-12-02 19:33:49 +0100

    Add 2016 day 2

---
+++ 2016/day02/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+int part1()
+{
+    std::ifstream file = std::fopen("input.txt", "r");
+    return -1;
+}
+int part2()
+{
+    return -1;
+}
+
+int main()
+{
+    std::cout << "Answer for part one: " << part1() << std::endl;
+    std::cout << "Answer for part two: " << part2() << std::endl;
+    return 0;
+}
