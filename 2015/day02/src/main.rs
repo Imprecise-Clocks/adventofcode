8b86ce52472d7a77f725e70287208cb54fb344bc
Benjamin Halilovic <bhalilovic.dev@gmail.com>
2026-09-19 23:28:43 +0200

    Code a bit in rust

--- 2015/day02/src/main.rs
+++ 2015/day02/src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+
+fn part1() -> i32 {
+    let file: String = fs::read_to_string("input.txt").expect("File not found!");
+    let mut area: i32 = 0;
+    for line in file.lines() {
+        let dimensions = line.split("x").map(|x| x.parse().unwrap()).collect();
+        let [l, w, h] = dimensions;
+        area += 2 * l * w + 2 * w * h + 2 * l * h;
+    }
+    area
+}
+
+fn part2() -> i32 {
+    -1
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("The answer to part one: {}", part1());
+    println!("The answer to part two: {}", part2());
 }
